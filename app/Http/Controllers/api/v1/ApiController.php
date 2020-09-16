0ef6424c760569ec6f2f06a4aabc7dd9c3bc3e9b
Ntonsite Mwamlima <mwamlimantonsite@gmail.com>
2020-09-16 14:54:45 +0300

    ApiController Done

--- app/Http/Controllers/api/v1/ApiController.php
+++ app/Http/Controllers/api/v1/ApiController.php
@@ -23,4 +23,37 @@ class ApiController extends Controller
         return response(['user'=> Auth::user(), 'access_token' => $accessToken]);
 
     }
+
+    public function register (Request $request) {
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6|confirmed',
+        ]);
+
+        if ($validator->fails())
+        {
+            return response(['errors'=>$validator->errors()->all()], 422);
+        }
+
+        $request['password']=Hash::make($request['password']);
+        $user = User::create($request->toArray());
+
+        $token = $user->createToken('Access Grant Client')->accessToken;
+        $response = ['token' => $token];
+
+        return response($response, 200);
+
+    }
+
+    public function logout (Request $request) {
+
+        $token = $request->user()->token();
+        $token->revoke();
+
+        $response = 'You have been succesfully logged out!';
+        return response($response, 200);
+
+    }
 }
